dbcca9a7943183db0ba8ab97961e554daca1d170
joaosouzaaa <joaoasouza982@gmail.com>
2024-04-02 18:30:48 -0300

    Feat: Adding Options Pattern.

---
+++ OptionsPattern.API/DependencyInjection/OptionsDependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using OptionsPattern.API.Constants;
+using OptionsPattern.API.Options;
+
+namespace OptionsPattern.API.DependencyInjection;
+
+internal static class OptionsDependencyInjection
+{
+    internal static void AddOptionsDependencyInjection(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<EmailCredentialsOptions>(configuration.GetSection(OptionsConstants.EmailCredentialsSection));
+    }
+}
