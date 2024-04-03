b00b6893e68791d20fff3809cda893890181b839
joaosouzaaa <joaoasouza982@gmail.com>
2024-04-02 21:07:49 -0300

    Feat: Adding EmailController.

---
+++ OptionsPattern.API/Controllers/EmailController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OptionsPattern.API.Interfaces.Services;
+
+namespace OptionsPattern.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public sealed class EmailController(IEmailService emailService) : ControllerBase
+{
+    [HttpPost("send-email")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public Task<bool> SendEmailAsync([FromQuery] string email) =>
+        emailService.SendEmailAsync(email);
+}
